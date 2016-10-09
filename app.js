679207271c18a29a40b4fb7067b42f6e6b2c6fe3
Stellan Söderström <stellan.soderstrom@gmail.com>
2016-10-10 00:04:37 +0200

    Changed port to 32000

--- app.js
+++ app.js
@@ -132,6 +132,6 @@ app.post('/upload', function (req, res) {
 
 });
 
-var server = app.listen(3000, function () {
-    console.log('Server listening on port 3000');
+var server = app.listen(32000, function () {
+    console.log('Server listening on port 32000');
 });
